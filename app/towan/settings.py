9b781bfee40e19cc7a4b9e60564b2fd84472e499
Lola Rigaut-Luczak <rllola80@gmail.com>
2017-05-21 15:53:49 +0200

    Added traefik reverse proxy

--- app/towan/settings.py
+++ app/towan/settings.py
@@ -109,11 +109,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 # tastypie
 TASTYPIE_DEFAULT_FORMATS = ['json']
