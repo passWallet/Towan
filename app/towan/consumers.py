35ffc952089960b8ba5c5449132ec5a8d1ada4eb
rllola <rllola80@gmail.com>
2017-05-28 19:26:20 +0200

    Fix websocket

--- app/towan/consumers.py
+++ app/towan/consumers.py
@@ -1,10 +1,16 @@
 from channels.sessions import channel_session
 from channels import Group
+from urllib import parse
 
 # Connected to websocket.connect
 @channel_session
-def ws_connect(message, address):
+def ws_connect(message):
+    query = parse.parse_qs(message['query_string'])
+    if 'address' not in query:
+        return
+    address = query['address'][0]
     message.channel_session['address'] = address
+    message.reply_channel.send({"accept": true})
     # Add connection to address group (should have only one connection)
     Group(address).add(message.reply_channel)
 
--- app/towan/routing.py
+++ app/towan/routing.py
@@ -2,6 +2,6 @@ from channels.routing import route
 from towan.consumers import ws_connect, ws_disconnect
 
 channel_routing = [
-    route("websocket.connect", ws_connect, path=r"^/(?P<address>[a-zA-Z0-9_]+)/$"),
+    route("websocket.connect", ws_connect),
     route("websocket.disconnect", ws_disconnect)
 ]
